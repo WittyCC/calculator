d41be81b4ed19d4e0a1c2b5f7ebd59fdecb485cd
Emilie Thoreson and Witty Chang <emilie@>
2017-05-31 15:29:24 -0700

    now feature subtract, multiply and divide user interface

--- js/scripts.js
+++ js/scripts.js
@@ -2,64 +2,79 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
-
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
-alert(result);
 
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
-
 var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#addoutput").text(result);
+  });
 
 
-var BMI = function(userweight, userheight){
-  return userweight/Math.pow(userheight, 2);
-};
-var weight = parseInt(prompt("Enter a weight number:"));
-var height = parseInt(prompt("Enter a height number:"));
-
-var result = BMI(weight, height);
-alert(result);
-
-var temp = function(usertemp){
-  return usertemp * 1.8 + 32
-}
-
- var celc = parseInt(prompt("Enter Celcium:"));
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#subtractoutput").text(result);
+  });
 
 
- var result = temp(celc);
- alert("Faringeit temp" + result);
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#multiplyoutput").text(result);
+  });
 
- var volGal = function(userLit){
-  return userLit /4
-}
 
- var Gal = parseInt(prompt("Enter volume in liters:"));
-
-
- var result = volGal(Gal);
- alert("Volume in gallons is equal to: " + result);
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#divideoutput").text(result);
+  });
+});
+//var BMI = function(userweight, userheight){
+//  return userweight/Math.pow(userheight, 2);
+//};
+// var weight = parseInt(prompt("Enter a weight number:"));
+// var height = parseInt(prompt("Enter a height number:"));
+//
+// var result = BMI(weight, height);
+// alert(result);
+//
+// var temp = function(usertemp){
+//   return usertemp * 1.8 + 32
+// }
+//
+//  var celc = parseInt(prompt("Enter Celcium:"));
+//
+//
+//  var result = temp(celc);
+//  alert("Faringeit temp" + result);
+//
+//  var volGal = function(userLit){
+//   return userLit /4
+// }
+//
+//  var Gal = parseInt(prompt("Enter volume in liters:"));
+//
+//
+//  var result = volGal(Gal);
+//  alert("Volume in gallons is equal to: " + result);
